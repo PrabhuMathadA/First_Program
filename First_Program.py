34287330053f6f80e5ee623d2417e9112ea10555
PrabhuMathadA <151036630+PrabhuMathadA@users.noreply.github.com>
2023-12-27 18:50:56 +0530

    Add files via upload

---
+++ First_Program.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("Hello World")
+
+
+# In[2]:
+
+
+Print("Pranhu Mathad")
+
+
+# In[3]:
+
+
+print("Pranhu Mathad")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
